1080b845bc823eed4d4d4b56b95107e830ab2078
leoxshn <leo@posidon.io>
2021-06-28 20:01:55 +0200

    fixed an embarrassing bug

--- app/src/main/java/io/posidon/android/launcherutils/demo/AppCollection.kt
+++ app/src/main/java/io/posidon/android/launcherutils/demo/AppCollection.kt
@@ -1,15 +1,16 @@
 package io.posidon.android.launcherutils.demo
 
+import android.content.Context
 import io.posidon.android.launcherutils.AppLoader
 
 class AppCollection(size: Int) : AppLoader.AppCollection<App> {
     val list = ArrayList<App>(size)
 
-    override fun add(app: App) {
+    override fun add(context: Context, app: App) {
         list.add(app)
     }
 
-    override fun finalize() {
+    override fun finalize(context: Context) {
         list.sortWith { o1, o2 ->
             o1.label.compareTo(o2.label, ignoreCase = true)
         }
